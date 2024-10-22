258635f4cfe4ceefd5c930459cda9695b763d3b5
santhoshjudah <santhoshbenhur777@gmail.com>
2024-10-22 12:18:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+let button = document.getElementById('add')
+let input = document.getElementById('input')
+let todolist = document.getElementById('todoList');
+
+// easy to delete
+let todos = [];
+window.onload = ()=>{
+  todos = JSON.parse(localStorage.getItem('todos')) || []
+  todos.forEach(todo=>addtodo(todo))
+}
+
+button.addEventListener('click',()=>{
+  todos.push(input.value)
+  localStorage.setItem('todos',JSON.stringify(todos))
+  addtodo(input.value)
+  input.value=''
+
+})
+
+function addtodo(todo){
+  let para= document.createElement('p')
+  para.innerText = todo;
+
+  todoList.appendChild(para)
+  para.addEventListener('click',()=>{
+     para.style.textDecoration = 'line-through'
+     remove(todo)
+  })
+  para.addEventListener('dblclick',()=>{
+    todoList.removeChild(para)
+    remove(todo)
+ })
+
+}
+function remove(todo){
+  let index = todos.indexOf(todo)
+  if(index>-1){
+    todos.splice(index,1);
+  }
+   
+  // localStorage.setItem('todos',JSON.stringify(todos))
+}
